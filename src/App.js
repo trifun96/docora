e3fae88ea071bd80c16ea134b65361eea6e1bb82
Trifun <trifun90@gmail.com>
2025-06-05 16:09:32 +0200

    updating css

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import React, { useState } from 'react';
-import PatientForm from './components/PatientForm';
-import ReportDisplay from './components/ReportDisplay';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './users/components/LandingPage/Background';
+import LoginPage from './users/components/LoginPage/LoginPage';
 
 function App() {
-  const [report, setReport] = useState('');
-  const [email, setEmail] = useState('');
-  const [patientData, setPatientData] = useState('');
-
   return (
-    <div style={{ maxWidth: 600, margin: 'auto', padding: 20, fontFamily: 'Arial, sans-serif' }}>
-      <h3 style={{ textAlign: 'center', fontSize:'18px', fontWeight:'200' }}>Docora - NextGen izveštaj za stomatologe</h3>
-      <PatientForm
-        onGenerateReport={setReport}
-        onEmailChange={setEmail}
-        onPatientDataFilled={setPatientData}
-      />
-      {report && <ReportDisplay report={report} email={email} patientData={patientData} />}
-    </div>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
   );
 }
 
