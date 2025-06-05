d1469b169eb5637b36963d1d7cc922f8573b09bc
Trifun <trifun90@gmail.com>
2025-06-05 16:28:58 +0200

    adding necessary code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import LandingPage from './users/components/LandingPage/Background';
 import LoginPage from './users/components/LoginPage/LoginPage';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<LandingPage />} />
+      <Route path="/" element={<Navigate to="/docora" replace />} />
+      
+      <Route path="/docora" element={<LandingPage />} />
       <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
